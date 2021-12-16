f7c761f219b5e7759f1d4d93ef82d07617a7878c
Kiall Thompson-Lane <kiall.thompson-lane@unity3d.com>
2021-12-16 16:08:27 +0000

    Day 5 part 1

---
+++ HydrothermalVenture/Grid.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace MyApp
+{
+    public class Grid
+    {
+        public Dictionary<coordinate, int> grid;
+        public Grid()
+        {
+            grid = new();
+        }
+
+        public void MarkVent(coordinate start, coordinate end)
+        {
+            //check if horizontal
+            if (start.y == end.y)
+            {
+                if (start.x < end.x)
+                {
+                    //going left to right
+                    int distance = end.x - start.x;
+                    for (int i = 0; i <= distance; i++)
+                    {
+                        coordinate coord = new();
+                        coord.x = start.x + i;
+                        coord.y = start.y;
+
+                        if (grid.ContainsKey(coord))
+                        {
+                            grid[coord] +=1;
+                        }
+                        else
+                        {
+                            grid.Add(coord, 1);
+                        }
+                    }
+                }
+                else
+                {
+                    //right to left
+                    int distance = start.x - end.x;
+                    for (int i = 0; i <= distance; i++)
+                    {
+                        coordinate coord = new();
+                        coord.x = start.x - i;
+                        coord.y = start.y;
+
+                        if (grid.ContainsKey(coord))
+                        {
+                            grid[coord] +=1;
+                        }
+                        else
+                        {
+                            grid.Add(coord, 1);
+                        }
+                    }
+                }
+            }
+            else if(start.x == end.x)
+            {
+                if (start.y < end.y)
+                {
+                    //going bottom up
+                    int distance = end.y - start.y;
+                    for (int i = 0; i <= distance; i++)
+                    {
+                        coordinate coord = new();
+                        coord.y = start.y + i;
+                        coord.x = start.x;
+
+                        if (grid.ContainsKey(coord))
+                        {
+                            grid[coord] +=1;
+                        }
+                        else
+                        {
+                            grid.Add(coord, 1);
+                        }
+                    }
+                }
+                else
+                {
+                    //top down
+                    int distance = start.y - end.y;
+                    for (int i = 0; i <= distance; i++)
+                    {
+                        coordinate coord = new();
+                        coord.y = start.y - i;
+                        coord.x = start.x;
+
+                        if (grid.ContainsKey(coord))
+                        {
+                            grid[coord] +=1;
+                        }
+                        else
+                        {
+                            grid.Add(coord, 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        public int HowManyOverlaps()
+        {
+            return grid.Count(c => c.Value >= 2);
+        }
+    }
+
+    public struct coordinate
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
---
+++ HydrothermalVenture/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        private static Grid grid = new Grid();
+        public static void Main(string[] args)
+        {
+            ParseInput("./input.txt");
+            Console.WriteLine(grid.HowManyOverlaps());
+        }
+
+        public static void ParseInput(string path)
+        {
+            var lines = File.ReadAllLines(path);
+            foreach (var line in lines)
+            {
+                var coordinates = line.Split(" -> ");
+                var startCoord = coordinates[0].Split(",");
+                coordinate start = new coordinate();
+                start.x = int.Parse(startCoord[0]);
+                start.y = int.Parse(startCoord[1]);
+                var endCoord = coordinates[1].Split(",");
+                coordinate end = new coordinate();
+                end.x = int.Parse(endCoord[0]);
+                end.y = int.Parse(endCoord[1]);
+                grid.MarkVent(start, end);
+            }
+        }
+    }
+}
